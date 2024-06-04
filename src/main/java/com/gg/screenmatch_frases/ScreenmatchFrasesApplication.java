14c2a573b04705d283306714a7d769a3372b078e
Gustavo Oli <69ggustavoo69@gmail.com>
2024-06-04 19:45:16 -0300

    first commit

---
+++ src/main/java/com/gg/screenmatch_frases/ScreenmatchFrasesApplication.java
@@ -0,0 +1,15 @@
+package com.gg.screenmatch_frases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchFrasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchFrasesApplication.class, args);
+
+
+	}
+
+}
